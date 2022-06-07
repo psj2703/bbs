1133cde38d8a18f44c29e8518763db663934b91f
seongjae <seongjae@seongjaeui-iMac.local>
2022-06-07 18:22:05 +0900

    user목록을 test에 넣어서 처리

--- repository/users.js
+++ repository/users.js
@@ -1,21 +1,81 @@
-const { isGeneratorFunction } = require("util/types")
+const fs = require('fs');
 
-const findAll = () => {
+const userData = fs.readFileSync('repository/test.json', "utf8")
+const users = JSON.parse(userData);
 
-}
+const column = {
+    findAll : function (fvalue) {
+        return users.user;
+    },
+    
+    findOne : function (fvalue) {
+        const result = users.user.find( element => element.id == fvalue);
+        return result;
+    },
+    
+    insert : function (req) {
+        const ids = users.user.map(user => user.id)
+        const maxId = Math.max(...ids)
+      
+        const { body } = req
+        const { name, age } = body
+      
+        const newUser = {
+          "id": maxId + 1,
+          "name": name,
+          "age": age
+        }
+        users.user.push(newUser);
 
-const findOne = () => {
+        console.log(users);
+        const userJSON = JSON.stringify(users)
+        console.log();
 
-}
+        fs.writeFile("./repository/test.json", userJSON, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
 
-const insert = () => {
+        return newUser;
+    },
+    
+    remove : function (fvalue) {
+        const found = users.user.findIndex(element => element.id == fvalue);
+        users.user.splice(found, 1);
 
-}
+        const userJSON = JSON.stringify(users)
 
-const remove = () => {
+        fs.writeFile("./repository/test.json", userJSON, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        return users.user;
+    },
+    
+    update : function (req, fvalue) {
+        const found = users.user.findIndex(element => element.id == fvalue);
+        const newUser = {
+          "id": Number(fvalue),
+          "name": req.body.name,
+          "age": req.body.age
+        }
+        users.user.splice(found, 1, newUser);
 
-}
+        const userJSON = JSON.stringify(users)
 
-const update = () => {
+        fs.writeFile("./repository/test.json", userJSON, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+
+        return users.user;
+    }
     
 }
+
+
+
+module.exports = column;
--- routes/users.js
+++ routes/users.js
@@ -2,70 +2,47 @@ const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 
-const users = [
-  {
-    id: 101,
-    name: 'hyeonjae',
-    age: 35,
-  },{
-    id: 102,
-    name: 'jinseok',
-    age: 35,
-  },{
-    id: 103,
-    name: 'dongil',
-    age: 41,
-  },{
-    id: 104,
-    name: 'seongjae',
-    age: 38,
-  },
-]
+var column = require('../repository/users.js');
+
+// const users = [
+//   {
+//     id: 101,
+//     name: 'hyeonjae',
+//     age: 35,
+//   },{
+//     id: 102,
+//     name: 'jinseok',
+//     age: 35,
+//   },{
+//     id: 103,
+//     name: 'dongil',
+//     age: 41,
+//   },{
+//     id: 104,
+//     name: 'seongjae',
+//     age: 38,
+//   },
+// ]
 
 router.get('/:id', (req, res, next) => {
-  const fvalue = req.params.id;
-  const found = users.find(element => element.id == fvalue);
-  res.send(found); 
+  const users = column.findOne(req.params.id)
+  res.send(users);
 });
 
 router.post('/', (req, res, next) => {
-  const ids = users.map(user => user.id)
-  const maxId = Math.max(...ids)
-
-  const { body } = req
-  const { name, age } = body
-
-  const newUser = {
-    id: maxId + 1,
-    name,
-    age,
-  }
-
-  users.push(newUser);
-
+  const newUser = column.insert(req);
   res.json(newUser);
-})
+});
 
 
 router.delete('/:id', (req, res, next) => {
-  const fvalue = req.params.id;
-  const found = users.findIndex(element => element.id == fvalue);
-  users.splice(found, 1);
-
+  const users = column.remove(req.params.id);
   res.json(users);  
 });
 
 router.put('/:id', (req, res, next) => {
-  const fvalue = req.params.id;
-  const found = users.findIndex(element => element.id == fvalue);
-  const newUser = {
-    id: Number(fvalue),
-    name: req.body.name,
-    age: req.body.age
-  }
-  users.splice(found, 1, newUser);
-
-  res.json(users);s
+  const users = column.update(req, req.params.id);
+  res.json(users);
 });
 
 
@@ -73,6 +50,7 @@ router.put('/:id', (req, res, next) => {
 router.get('/', function(req, res, next) {
   console.log('GET users listing')
   // res.send("{\"name\":\"hyeonjae\"}");
+  const users = column.findAll();
   res.json(users);
 });
 
